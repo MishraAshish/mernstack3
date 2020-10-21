205685b66ffa112ef91f7d37077bfa11a81fdbac
Ashish <ash.ishu025g@gmail.com>
2020-10-21 23:55:39 +0530

    test

---
+++ ES6/Test.js
@@ -0,0 +1,20 @@
+//Problem 2
+console.log("1" == true); //true 
+
+//Problem 3
+console.log("01" == true); //true
+
+//Problem 4
+console.log("01" == "1"); //false
+
+//Problem 5
+console.log("01" == 1); //true
+
+//Problem 6
+
+console.log([] == ![]); //true 
+
+console.log(typeof []) 
+console.log(typeof ![]) 
+console.log(![]) //not operator is - false
+console.log([].length) 
