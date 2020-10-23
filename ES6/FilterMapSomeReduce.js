ab11c9a2094dff21e353f498e6ec1ab825d63e13
Ashish <ash.ishu025g@gmail.com>
2020-10-23 21:18:05 +0530

    iterators

--- ES6/FilterMapSomeReduce.js
+++ ES6/FilterMapSomeReduce.js
@@ -47,3 +47,39 @@ let numberOfPersonsSavedByEachSuperHero = personsList.reduce((preViousVal, curre
 
 console.log("Saved By Each", numberOfPersonsSavedByEachSuperHero)
 //console.log("Core List", personsList);
+
+
+//1. Print the persons saved by IronMan
+//2. Print the person names saved by IronMan
+//3. Check if anyone saved by Hulk
+
+//Practice Questions : 
+let persons = [
+    {id : 1, name : "Allen", tags : "javascript"},
+    {id : 2, name : "Ram", tags : "javascript"},
+    {id : 3, name : "Vennis", tags : "java"},
+    {id : 4, name : "Scott", tags : "javascript"},
+    {id : 5, name : "Alex", tags : "java"}
+];
+
+//1. Name all having tag javascript
+//2. List all objcts having tag java
+//3. Check for any "c-sharp" tag present
+//4. Count the number of people having tag javascript and java
+
+// How to preserve the immutability on my heroes list?
+// 1. Get heroes who are not evils
+// 2. Print Unique family names
+// 3. Print Hero Names from given objects, and append sir in each of them before printing
+
+const heroes = [
+    { name: 'Wolverine',      family: 'Marvel',    isEvil: false },
+    { name: 'Deadpool',       family: 'Marvel',    isEvil: false },
+    { name: 'Magneto',        family: 'Marvel',    isEvil: true  },
+    { name: 'Charles Xavier', family: 'Marvel',    isEvil: false },
+    { name: 'Batman',         family: 'DC Comics', isEvil: false },
+    { name: 'Harley Quinn',   family: 'DC Comics', isEvil: true  },
+    { name: 'Legolas',        family: 'Tolkien',   isEvil: false },
+    { name: 'Gandalf',        family: 'Tolkien',   isEvil: false },
+    { name: 'Saruman',        family: 'Tolkien',   isEvil: true  }
+  ]
