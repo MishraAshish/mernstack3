2fb03c786a8ebf9e7c9fa92fca7b59bfa95b3c1a
Ashish <ash.ishu025g@gmail.com>
2020-11-03 15:36:29 +0530

    webpack setup

---
+++ ReactApp/src/index.js
@@ -0,0 +1,7 @@
+//here onwards our application starts -very first page of our application
+console.log("The entry page that is index.js has been loaded");
+console.log("The entry page that is index.js has been loaded1");
+console.log("The entry page that is index.js has been loaded2");
+console.log("The entry page that is index.js has been loaded3");
+console.log("The entry page that is index.js has been loaded4");
+console.log("The entry page that is index.js has been loaded5");
