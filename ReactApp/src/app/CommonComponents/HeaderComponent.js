277c6ff81cbf62d86eb0e73c0835775374783017
Ashish <ash.ishu025g@gmail.com>
2020-11-19 00:24:32 +0530

    header conditions

--- ReactApp/src/app/CommonComponents/HeaderComponent.js
+++ ReactApp/src/app/CommonComponents/HeaderComponent.js
@@ -2,14 +2,22 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import {connect} from "react-redux";
 
+//Show only Home, User and About for the users not logged in and show all tabs when user logs in so need to render navigations conditionally
 const Header = (props)=>{
+    let userName = props.user.userName;
     return(
         <React.Fragment>
-            <h1>{props.user.userName}</h1>
+             Hi <b>{userName +", "}</b> Welcome to SynergisticIT Shopping Cart 
+                {userName == "" ?<b> Please Login to see other features</b>:""}
+            <hr/>
             <NavLink to="/home" className="button" activeClassName="success" >Home </NavLink> 
             <NavLink to="/user" className="button" activeClassName="success" >User </NavLink> 
+            {userName ?
+            <React.Fragment>
             <NavLink to="/product" className="button" activeClassName="success" >Product </NavLink> 
             <NavLink to="/cart" className="button" activeClassName="success" >Cart </NavLink> 
+            </React.Fragment> : ""
+            }
             <NavLink to="/about" className="button" activeClassName="success">About  </NavLink>
         </React.Fragment>
     )
